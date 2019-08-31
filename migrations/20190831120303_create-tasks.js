0cd8f2e34bb2045be5d9adb27e1b8e829eab31d0
Matt Hagner <web@matthagner.com>
2019-08-31 12:09:56 -0500

    Create task model and seeds

---
+++ migrations/20190831120303_create-tasks.js
@@ -0,0 +1,34 @@
+/*
+ * Task {
+ *   id: ID! (@auto increments)
+ *   description: String!
+ *   notes: String
+ *   project_id: ID! (@foreign_key = projects, @constraint = must_exist)
+ *   completed: Boolean! (@default = false = 0)
+ * }
+ *
+ */
+
+exports.up = function(knex) {
+  return knex.schema.createTable("tasks", tbl => {
+    tbl.increments(); // Primary key
+
+    tbl.text("description").notNullable();
+    tbl.text("notes");
+
+    tbl
+      .integer("project_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("projects")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+
+    tbl.boolean("completed").defaultTo(false);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("tasks");
+};
---
+++ seeds/03-tasks.js
@@ -0,0 +1,21 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('tasks').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('tasks').insert([
+        {id: 1, description: "task-1", notes: "task-1-notes", project_id: 1, completed: false},
+        {id: 2, description: "task-2", notes: "task-2-notes", project_id: 1, },
+        {id: 3, description: "task-3", project_id: 1, completed: true},
+
+        {id: 4, description: "task-4", notes: "task-4-notes", project_id: 2, completed: false},
+        {id: 5, description: "task-5", notes: "task-5-notes", project_id: 2, },
+        {id: 6, description: "task-6", project_id: 2, completed: true},
+
+        {id: 7, description: "task-7", notes: "task-7-notes", project_id: 3, completed: false},
+        {id: 8, description: "task-8", notes: "task-8-notes", project_id: 3, },
+        {id: 9, description: "task-9", project_id: 3, completed: true},
+      ]);
+    });
+};
