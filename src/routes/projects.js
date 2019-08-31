3080fc398b56c76e0b5d5c69c09aba169dafe2d1
Matt Hagner <web@matthagner.com>
2019-08-31 12:25:09 -0500

    Set up routes for server

---
+++ src/routes/projects.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+
+router.get("/", (req, res) => {});
+router.post("/", (req, res) => {});
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,4 +1,10 @@
-const server = require("express")();
+const express = require("express");
+const projectsRoute = require("./routes/projects");
+
+const server = express();
+
+server.use(express.json());
+server.use("/api/projects", projectsRoute);
 
 server.get("/", (_, res) => res.json({ message: "Hello, World" }));
 
